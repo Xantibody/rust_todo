458494aa4cff98f945fe7a074b18fd88929838fa
Xantibody <zeku.bushinryu38@gmail.com>
2024-11-12 07:33:16 +0900

    implement is md file

--- rust_todo/src/main.rs
+++ rust_todo/src/main.rs
@@ -1,4 +1,6 @@
 use clap::Parser;
+use std::path::Path;
+
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
@@ -9,5 +11,11 @@ fn main() {
     let args: Args = Args::parse();
     for v in &args.quotes {
         println!("print arg -> {}", v);
+    }
+
+    if Path::new("~/.todo/todo.md").is_file() {
+        print!("ある!")
+    } else {
+        print!("ない!")
     }
 }
